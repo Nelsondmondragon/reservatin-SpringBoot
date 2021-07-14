0468cb8f6913c8f7278eefc8904fd6de24d7c245
Nelson Cortes <nelsondmondragon@gmail.com>
2021-07-13 22:22:09 -0500

    Implementado capa de servicios

--- src/main/java/com/platzi/ereservation/services/ClientService.java
+++ src/main/java/com/platzi/ereservation/services/ClientService.java
@@ -1,5 +1,65 @@
 package com.platzi.ereservation.services;
 
+import org.springframework.stereotype.Service;
+
+import com.platzi.ereservation.model.Client;
+import com.platzi.ereservation.repository.clientRepository;
+
+/**
+ * Class to define client services.
+ * 
+ * @author Nelson
+ *
+ */
+
+@Service
 public class ClientService {
+
+	private final clientRepository clientRepository;
+
+	public ClientService(clientRepository clientRepository) {
+		this.clientRepository = clientRepository;
+	}
+
+	/**
+	 * Method to perform the operation of saving client
+	 * 
+	 * @param client
+	 * @return
+	 */
+
+	public Client create(Client client) {
+		return this.clientRepository.save(client);
+	}
+
+	/**
+	 * Method to perform the operation of update client.
+	 * 
+	 * @param client
+	 * @return
+	 */
+	public Client update(Client client) {
+		return this.clientRepository.save(client);
+	}
+
+	/**
+	 * Method to perform the operation of delete client.
+	 * 
+	 * @param client
+	 */
+	public void delete(Client client) {
+		this.clientRepository.delete(client);
+
+	}
+
+	/**
+	 * Method to consult a client by identification.
+	 * 
+	 * @param identification
+	 * @return
+	 */
+	public Client finByIdentification(String identification) {
+		return this.clientRepository.findByIdentification(identification);
+	}
 
 }
